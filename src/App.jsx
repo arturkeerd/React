abedc49b1505454957369b5db04e283fc921200d
Artur Keerd <arturkeerd@gmail.com>
2025-01-23 13:16:50 +0200

    added data and made it readable

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,37 @@ import React from 'react'
 import ExpenseItem from './components/ExpenseItem.jsx'
 
 const App = () => {
+
+  const expenses = [
+    {
+      date : new Date(2024, 10, 12),
+      title : 'Jonagold',
+      price : 2.49
+    },
+    {
+      date : new Date(2024, 10, 12),
+      title : 'Granny Smith',
+      price : 2.99
+    },
+    {
+      date : new Date(2024, 10, 12),
+      title : 'Eesti segu',
+      price : 3.49
+    },
+    {
+      date : new Date(2024, 10, 12),
+      title : 'Polish Red',
+      price : 0.99
+    }
+  ]
+
+
   return (
     <div className="App">
-      <ExpenseItem />
-  
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
+      <ExpenseItem data={expenses[2]}/>
+      <ExpenseItem data={expenses[3]}/>
     </div>
   )
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,19 @@
 import './ExpenseItem.css'
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+    const date = new Date(2024, 10, 12)
+    const title = 'New book'
+    const price = 30.99
+
+    console.log(props)
+    console.log(props.data)
+
     return (
         <div className='expense-item'>
-            <div>Date</div>
+            <div>{props.data.date.toString()}</div>
             <div className='expense-item__description'>
-                <h2>ExpenseItem</h2>
-                <div className='expense-item__price'>Price</div>
+                <h2>{props.data.title}</h2>
+                <div className='expense-item__price'>{props.data.price}</div>
             </div>
         </div>
     )
