403fc968a3faed03094cf30dc435ade077fc97a9
Artur Keerd <arturkeerd@gmail.com>
2025-01-24 13:20:44 +0200

    Event Listen and Handle(Leht10)

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,8 +1,17 @@
 import ExpenseDate from './ExpenseDate.jsx'
 import './ExpenseItem.css'
 import Card from '../UI/Card.jsx'
+import { useState } from 'react'
+
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.data.title)
+
+    const clickHandler = () => {
+        console.log('Clicked!')
+        setTitle(`Updated by click ${title}`)
+        console.log(title)
+    }
 
     return (
         <Card className='expense-item'>
@@ -11,6 +20,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
             </div>
+            <button onClick={clickHandler}>Click Me</button>
         </Card>
     )
 }
