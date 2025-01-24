b3793aafc49946ce775673c7702aee344a80ebf5
Artur Keerd <arturkeerd@gmail.com>
2025-01-24 12:27:28 +0200

    Added Card and consolidated style (Leht6)

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,14 +1,15 @@
 import ExpenseItem from './ExpenseItem.jsx'
+import Card from './Card'
 import './Expenses.css'
 
 const Expenses = (props) => {
     return (
-    <div className='expenses'>
+    <Card className='expenses'>
       <ExpenseItem data={props.expenses[0]}/>
       <ExpenseItem data={props.expenses[1]}/>
       <ExpenseItem data={props.expenses[2]}/>
       <ExpenseItem data={props.expenses[3]}/>
-    </div>
+    </Card>
     )
 }
 
