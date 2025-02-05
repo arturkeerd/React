7e21a72432f11ff1d82a53e19b778916f921bde3
Artur Keerd <arturkeerd@gmail.com>
2025-02-05 12:57:23 +0200

    added Year filter

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
+import ExpensesFilter from './ExpensesFilter.jsx'
 import ExpenseItem from './ExpenseItem.jsx'
 import Card from '../UI/Card.jsx'
 import './Expenses.css'
 
 const Expenses = (props) => {
+
+  const [filteredYear, setFilteredYear] = useState('2024')
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(`Your data in Expenses.jsx ${selectedYear}`);
+  }
+
     return (
     <Card className='expenses'>
+      <ExpensesFilter selectedYear={filteredYear} onChangeFilter={filterChangeHandler} />
+
       <ExpenseItem data={props.expenses[0]}/>
       <ExpenseItem data={props.expenses[1]}/>
       <ExpenseItem data={props.expenses[2]}/>
